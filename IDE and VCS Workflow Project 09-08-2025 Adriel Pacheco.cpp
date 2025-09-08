a121f187b3d3b00eae826559402b8305fa6b6b78
Posei <Posei@ADRIELS-LAPTOP>
2025-09-08 10:51:42 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project 09-08-2025 Adriel Pacheco.cpp
@@ -0,0 +1,14 @@
+// IDE and VCS Workflow Project 09-08-2025 Adriel Pacheco.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+	char first, middle, last;
+	// Store your initials in these variables
+	first = 'A';
+	middle = 'N';
+	last = 'P'; 
+	std::cout << first << middle << last << __std_find_end_1;
+	return 0; 
+}
